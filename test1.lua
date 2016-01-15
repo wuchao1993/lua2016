fd4227185be832710629dadd3e3afb960378d98b
wuchao1993 <pro@proteki-MacBook-Pro.local>
2016-01-15 14:06:57 +0800

    2

--- test1.lua
+++ test1.lua
@@ -3,3 +3,16 @@ function sayHello()
 	print('hello world')
 end
 sayHello()
+
+
+function min(a,b)
+	if a>b then
+	    print(a)
+	end
+	    print(b)
+end
+min(100,20)
+
+for i=1,1000 do
+	print(i)
+end
